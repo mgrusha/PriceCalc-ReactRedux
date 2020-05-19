d465e23286d8f61cfbaa0e5a78427e9fec465238
mykytagrusha <mykyta.grusha@collibra.com>
2020-05-19 20:37:21 +0200

    Updates

---
+++ precache-manifest.918ea9ddb1617d0d551d2d1857158e28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68ba507cae5aa6e44195326e4aedc545",
+    "url": "/pricecalc-reactredux/index.html"
+  },
+  {
+    "revision": "c673d14851251f0fd268",
+    "url": "/pricecalc-reactredux/static/css/main.d52f5be8.chunk.css"
+  },
+  {
+    "revision": "6e411b8355522ef7a2a1",
+    "url": "/pricecalc-reactredux/static/js/2.53a9cb4b.chunk.js"
+  },
+  {
+    "revision": "3205f2f97be94286aaab611b41fbc37d",
+    "url": "/pricecalc-reactredux/static/js/2.53a9cb4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c673d14851251f0fd268",
+    "url": "/pricecalc-reactredux/static/js/main.31803727.chunk.js"
+  },
+  {
+    "revision": "b60cfd4c926bc9a25488",
+    "url": "/pricecalc-reactredux/static/js/runtime-main.1648e72b.js"
+  }
+]);
